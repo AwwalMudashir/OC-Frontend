ab0cd9df9f5ec542014504171cf938f726670dfe
AwwalMudashir <awwalmudashir2.0@gmail.com>
2026-05-03 13:53:57 +0100

    Improving Google SEO

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,28 @@ import Manifesto from "./components/Manifesto";
 import Footer from "./components/Footer";
 
 export default function Home() {
+  const jsonLd = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Muhammed Oroye',
+    alternateName: ['Muhammed Oroye Portfolio', 'MO'],
+    url: 'https://muhammedoroye.org',
+  }
+
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <AboutSection/>
-      <Manifesto/>
-      <Footer/>
+      {/* This invisible script tells search engines the official site name */}
+      <script
+        type="application/ld+json"
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+
+      <Navbar />
+      <Hero />
+      <AboutSection />
+      <Manifesto />
+      <Footer />
     </div>
   );
 }
